c1c00d99a54a27af643db118ddda3a60b20b421d
alkismavridis <Alkis.Mavridis@drei.com>
2021-01-03 22:52:14 +0100

    Setup frontend structure

---
+++ frontend/src/entities/Clade.ts
@@ -0,0 +1,10 @@
+export default class Clade {
+    id: string;
+    parentId: string;
+
+    name: string;
+    scientificName: string;
+    description: string;
+    age: number;
+    mainImageUrl: string;
+}
